8919a811ee89dd703ce3b1c6f05a4fd7fb1b6156
parkhr <vlhong94@gmail.com>
2021-10-21 21:03:31 +0900

    name change

---
+++ road-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello sb-nav-fixed">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  created() {
+    console.log(this.$store);
+    console.log(process.env);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
