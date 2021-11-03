304674cd2f004d1749c0bc2df2deeab0424a6421
parkhr <vlhong94@gmail.com>
2021-11-04 01:39:45 +0900

    feature: 공통 에러 구현

--- road-backend/src/app.module.ts
+++ road-backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users.module';
 import { TypeOrmConfig } from './config/typeorm';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './exception.filter';
 
 @Module({
   imports: [
@@ -11,6 +13,11 @@ import { TypeOrmConfig } from './config/typeorm';
     UsersModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule {}
