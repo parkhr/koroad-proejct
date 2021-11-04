a9c8e150b48fb58c1250768265103548671eba30
parkhr <vlhong94@gmail.com>
2021-11-04 23:50:26 +0900

    feat: custom repository 설정

--- road-backend/src/users.module.ts
+++ road-backend/src/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
+import { UserRepository } from './user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserRepository])],
   providers: [UsersService],
   controllers: [UsersController],
 })
--- road-backend/src/users.service.ts
+++ road-backend/src/users.service.ts
@@ -1,29 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { UserDto } from './user.dto';
 import { User } from './user.entity';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    private usersRepository: UserRepository,
   ) {}
 
   findAll(): Promise<User[]> {
     return this.usersRepository.find();
   }
 
-  findOne(id: string): Promise<User> {
-    return this.usersRepository.findOne(id);
-  }
-
   save(userDto: UserDto): Promise<User> {
     return this.usersRepository.save(User.createUser(userDto));
-  }
-
-  async remove(id: string): Promise<void> {
-    await this.usersRepository.delete(id);
   }
 }
